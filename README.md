6b39e4c5fab1c0451eceaf48bfaf3b9e6f5c23cb
mjahid98 <mdmujahidislam4@gmail.com>
2022-04-01 23:34:26 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ligspook-Miev
---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("keydown", function(e) {
+    if (e.key === 's' && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+      e.preventDefault();
+      document.querySelector('main').remove();
+    }
+  }, false);
+
+  document.oncontextmenu = document.body.oncontextmenu = function() {return false;}
